9b3e576785c3cfe18a81db231a166443d4998ed4
Valentina Morales <111003190+ValenMoraR@users.noreply.github.com>
2024-09-06 09:48:47 -0500

    Update Console.py

--- src/Console/Console.py
+++ src/Console/Console.py
@@ -1,8 +1,7 @@
 import sys
 sys.path.append("src")
-sys.path.append("src/LiquidacionNomina")
-from LiquidacionNomina import Liquida_nomina1, validations 
-
+from LiquidacionNomina import Liquida_nomina1
+6
 def validate_empty(value):
     return value if value else "0"
 
@@ -12,20 +11,20 @@ print("**NOTE: For data that does not apply to your profile, please leave it bla
 print("----------------------------------------------------------------------")
 monthly_salary = validate_empty(input("Enter your monthly income ($COP):"))
 weeks_worked = validate_empty(input("Enter the number of weeks worked to settle:"))
-holiday_hours_worked = validate_empty(input("Enter the hours worked on holidays (ONLY IF APPLICABLE):"))
+time_worked_on_holidays = validate_empty(input("Enter the hours worked on holidays (ONLY IF APPLICABLE):"))
 overtime_day_hours = validate_empty(input("Enter the number of daytime overtime hours worked (ONLY IF APPLICABLE):"))
 overtime_night_hours = validate_empty(input("Enter the number of nighttime overtime hours worked (ONLY IF APPLICABLE):"))
-holiday_overtime_hours = validate_empty(input("Enter the overtime hours worked on holidays (ONLY IF APPLICABLE):"))
+overtime_holiday_hours = validate_empty(input("Enter the overtime hours worked on holidays (ONLY IF APPLICABLE):"))
 leave_days = validate_empty(input("Enter the number of days you had on leave during the working period (ONLY IF APPLICABLE):"))
 sick_days = validate_empty(input("If you had any sick leave during the working period, enter the number of days (ONLY IF APPLICABLE):"))
 
-# try:
-liquidacion = Liquida_nomina1.Liquidacion(
-    monthly_salary, weeks_worked, holiday_hours_worked,
-    overtime_day_hours, overtime_night_hours, holiday_overtime_hours, leave_days, sick_days
-)
-result= liquidacion.CalcularLiquidacion()
-print(f"The total amount of your settlement is: {result}")
-# except Exception as up_error:
-    # print("*** ERROR ***")
-    # print(str(up_error))
+try:
+    liquidacion = Liquida_nomina1.Liquidacion(
+        monthly_salary, weeks_worked, time_worked_on_holidays,
+        overtime_day_hours, overtime_night_hours, overtime_holiday_hours, leave_days, sick_days
+    )
+    result= liquidacion.CalcularLiquidacion()
+    print(f"The total amount of your settlement is: {result}")
+except Exception as up_error:
+    print("*** ERROR ***")
+    print(str(up_error))
